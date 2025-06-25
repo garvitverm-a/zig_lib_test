6d82f7c3ea326a7dc4aafeb171031cc250fa1c1a
garvitverm-a <garvit.verma2512@gmail.com>
2025-06-25 16:23:46 +0530

    build change

--- build.zig
+++ build.zig
@@ -28,4 +28,6 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
+
+    b.addModule("testlib", lib_mod);
 }
